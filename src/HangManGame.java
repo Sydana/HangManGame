4d985190308170c62bf94e3d233861bb21af1426
Unknown <sydana.schalk.2019@student.lhschools.org>
2018-10-29 14:21:38 -0400

    Start

---
+++ src/HangManGame.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class HangManGame {
+    public static void main(String[] args) {
+        Scanner keyboard;
+        int score = 0;
+        String guessedLetter;
+        boolean value = true;
+        String playAgain = "";
+        String [] chosenWord = {"vein", "question", "sea", "beds", "beat", "scorch", "hug", "imply", "hot", "oven", "pour", "knowledge", "cook", "wry", "best", "stupid", "encircle", "ground", "assorted", "counsel"};
+
+
+
+        System.out.println("This is the Hang Man Game. These are the rules.");
+        System.out.println("There is going to be a word that you have to guess by guessing the letters that make up said word.");
+        System.out.println("You get fifteen tries to do so. If you win, you gain a point.\nYou could see your score at the end of the game, where you could choose to restart the game.");
+        System.out.println("Let's get started, guess a letter.");}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
