5b5703cc1af99925e72845f27472a1f347e8611e
Unknown <sydana.schalk.2019@student.lhschools.org>
2018-10-30 14:23:21 -0400

    I created loop for the word "pour"

--- src/HangManGame.java
+++ src/HangManGame.java
@@ -7,14 +7,23 @@ public class HangManGame {
         String guessedLetter;
         boolean value = true;
         String playAgain = "";
-        String [] chosenWord = {"vein", "question", "sea", "beds", "beat", "scorch", "hug", "imply", "hot", "oven", "pour", "knowledge", "cook", "wry", "best", "stupid", "encircle", "ground", "assorted", "counsel"};
-
-
+        String[] chosenWord = {"pour", "knowledge", "cook", "wry", "best"};
+        keyboard = new Scanner(System.in);
 
         System.out.println("This is the Hang Man Game. These are the rules.");
         System.out.println("There is going to be a word that you have to guess by guessing the letters that make up said word.");
         System.out.println("You get fifteen tries to do so. If you win, you gain a point.\nYou could see your score at the end of the game, where you could choose to restart the game.");
-        System.out.println("Let's get started, guess a letter.");}
+        System.out.println("Let's get started, guess a letter.");
+        guessedLetter = keyboard.nextLine();
+
+        while (value) {
+            if (chosenWord.equals("pour")){
+                if(guessedLetter.equals("p")||guessedLetter.equals("o")||guessedLetter.equals("u")||guessedLetter.equals("r")){
+
+
+
+                }
+            }
 
 
 
@@ -22,27 +31,7 @@ public class HangManGame {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+                        }
+    }
 }
+ public static String printMan(int tries, boolean win, String guessedLetter, String chosenWord){}
